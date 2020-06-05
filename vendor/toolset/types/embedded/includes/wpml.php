ae14190065385f1e91a846a0f23ed8f4f2a996ad
Christian Thomas <christian@industrious.agency>
2020-06-05 12:03:11 +0100

    Bug fix for PHP 7.3

--- vendor/toolset/types/embedded/includes/wpml.php
+++ vendor/toolset/types/embedded/includes/wpml.php
@@ -643,7 +643,7 @@ function wpcf_wpml_register_labels( $prefix, $data, $context = 'post' ) {
                 	$string_context = array( 'domain'  => 'Types-TAX' );
 	                $string_context['context'] = $label === 'name' ? 'taxonomy general name' : 'taxonomy singular name';
                     wpcf_translate_register_string( $string_context, false, $string );
-                    continue;
+                    break;
                 }
                 if ( isset( $default['labels'][$label] ) && $string == $default['labels'][$label] ) {
                     wpcf_translate_register_string( 'Types-TAX', $label, $string );
@@ -660,7 +660,7 @@ function wpcf_wpml_register_labels( $prefix, $data, $context = 'post' ) {
                 if ( $label == 'name' || $label == 'singular_name' ) {
                     wpcf_translate_register_string( 'Types-CPT',
                             $prefix . ' ' . $label, $string );
-                    continue;
+                    break;
                 }
 
                 // Check others for defaults
@@ -936,8 +936,8 @@ function wpcf_wpml_post_type_renamed( $new_slug, $old_slug ) {
 
 		$conflict_count = $wpdb->get_var(
 			$wpdb->prepare(
-				"SELECT COUNT(1) 
-				FROM {$icl_strings_table} AS s 
+				"SELECT COUNT(1)
+				FROM {$icl_strings_table} AS s
 				WHERE `name` LIKE %s AND `context` LIKE %s
 				LIMIT 1",
 				$new_string_name,
